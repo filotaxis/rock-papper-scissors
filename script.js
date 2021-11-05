4ea970e20ca021efe6e5a8881820d86241893b77
Bryan <bryan.anidem@gmail.com>
2021-11-04 19:26:05 -0600

    display images of winners and play again button functionallity

--- script.js
+++ script.js
@@ -4,32 +4,60 @@ const Game = (() => {
     const textDisplay = document.querySelector(".message>p");
     const stocksOponent = document.querySelector(".stocks-oponent");
     const stocksPlayer = document.querySelector(".stocks-player");
+    const displayBattle = document.querySelector(".display");
+    const displayWinner = document.querySelector(".winner-display");
+    const chooseButtons = document.querySelector(".choose");
+    const playAgain = document.querySelector(".play-again");
+
     const choicesType = ["grass", "fire", "water"];
-    
+
     let counterPlayer = 0;
     let counterComputer = 0;
     
-    const playSet = () => {
-            playRound();
-    }
 
-    const playRound = () => {
-        const choices = ["bulbasaur", "charmander", "squirtle"];
+    const play = () => {
+        const choicesRed = ["bulbasaur", "charmander", "squirtle"];
+        const choicesYellow = ["chikorita", "cyndaquill", "totodile"];
         const buttons = document.querySelectorAll("button");
         buttons.forEach(button => {
             button.addEventListener("click", () => {
             const human = parseInt(button.getAttribute("data-id"));   
             const computer = Math.floor(Math.random() * 3);
-            oponentDisplay.src = `images/${choices[computer]}_front.png`; 
-            playerDisplay.src = `images/${choices[human]}_back.png`;
+            oponentDisplay.src = `images/${choicesRed[computer]}_front.png`; 
+            playerDisplay.src = `images/${choicesYellow[human]}_back.png`;
             rules(human, computer);
-                console.log(`🖥️:${counterComputer} - 🤷‍♂️${counterPlayer}`);
                 if (counterComputer >= 3 || counterPlayer >= 3) {
-                    console.log("se acabó!");
+                    displayBattle.style.display = "none";
+                    chooseButtons.style.display = "none";
+                    displayWinner.style.display = "flex";
+                    playAgain.style.display = "flex";
+                    let winner = counterComputer > counterPlayer ? "OPONENT" : "YOU";
+                    let img = winner === "YOU" ? "yellow_front" : "red";
+                    const announce = document.createElement("p");
+                    const championImg = document.createElement("img");
+                    announce.textContent = `${winner} won!`;
+                    championImg.src = `images/${img}.png`;
+                    displayWinner.appendChild(announce);
+                    displayWinner.appendChild(championImg);
                 }
                             
             });
     
+        });
+
+        playAgain.addEventListener("click", () => {
+            displayBattle.style.display = "flex";
+            chooseButtons.style.display = "flex";
+            displayWinner.style.display = "none";
+            playAgain.style.display = "none";
+            oponentDisplay.src = "images/red.png"; 
+            playerDisplay.src = "images/yellow.png";
+            counterPlayer = 0;
+            counterComputer = 0;
+            stocksPlayer.innerHTML = "";
+            stocksOponent.innerHTML = "";
+            textDisplay.textContent = "RED trainer wants to battle with you!";
+            displayWinner.innerHTML = "";
         });
     }
 
@@ -55,11 +83,11 @@ const Game = (() => {
     }
 
     return {
-        playSet,
+        play,
     }
 })();
 
-Game.playSet();
+Game.play();
 
 
 
